55ff761451da38154c222a3322f9adeaf32c9164
Fei Luo <feifeigd@21cn.com>
2016-06-10 23:35:55 +0800

    Create main.cpp
    r-value_reference

---
+++ r-value_reference/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int g_constructCount = 0;
+int g_copyConstructCount = 0;
+int g_destructCount = 0;
+
+struct A{
+	A(){
+		cout << "construct: " << ++g_constructCount << endl;
+	}
+	
+	A(A const& a){
+		cout << "copy construct: " << ++g_copyConstructCount << endl;
+	}
+	
+	~A(){
+		cout << "destruct: " << ++g_destructCount << endl;
+	}
+};
+
+A GetA(){
+	return A();
+}
+
+int main(){
+	A a = GetA();
+	return 0;
+}
